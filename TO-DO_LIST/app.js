07917471c1b679571805539e9ab3ed12c2057297
Vishal Verma <v.verma3594@gmail.com>
2022-12-22 20:51:01 +0530

    TODO List

--- TO-DO_LIST/app.js
+++ TO-DO_LIST/app.js
@@ -1,39 +1,73 @@
-let formTask= document.querySelector('#formTask');
-let InputTask= document.querySelector('#InputTask');
-let listItem= document.querySelector('#listItem');
-formTask.addEventListener('submit', (event)=>{
-  event.preventDefault();
-  let InputTaskVal=InputTask.value;
-  let webTask= localStorage.getItem("localtask");
-  if(webTask== null){
-    taskObj=[];
-  }
-  else{
-    taskObj= JSON.parse(webTask);
-  }
-  taskObj.push(InputTaskVal);
-  localStorage.setItem("localtask", JSON.stringify(taskObj) );
-  taskShow()
-})
+let formTask = document.querySelector("#formTask");
+let InputTask = document.querySelector("#InputTask");
+let listItem = document.querySelector("#listItem");
+let updateBtn = document.querySelector("#update");
+let saveTask = document.querySelector("#saveTask");
+let addBtn = document.querySelector("#add");
 
-let taskShow= ()=>{
-  let webTask= localStorage.getItem("localtask");
-  if(webTask== null){
-    taskObj=[];
+// add to localStorage
+formTask.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let InputTaskVal = InputTask.value;
+  let webTask = localStorage.getItem("localtask");
+  if (InputTaskVal.trim() != 0) {
+    webTask == null ? (taskObj = []) : (taskObj = JSON.parse(webTask));
+    taskObj.push(InputTaskVal);
+    localStorage.setItem("localtask", JSON.stringify(taskObj));
+    InputTask.value = "";
+    taskShow();
   }
-  else{
-    taskObj= JSON.parse(webTask);
+});
+
+// display list
+let taskShow = () => {
+  let webTask = localStorage.getItem("localtask");
+  if (webTask == null) {
+    taskObj = [];
+  } else {
+    taskObj = JSON.parse(webTask);
   }
-  let listItem='';
+  let lists = "";
   taskObj.forEach((item, index) => {
-    listItem += `
+    lists += `
       <li>
-      <span>${index + 1}</span>
-      <span>${item}</span>
+      <span>${index + 1}. ${item}</span>
+      <span><i class="fa-solid fa-pen-to-square"  onclick="edittask(${index})"></i> &nbsp;
+      <i class="fa-solid fa-rectangle-xmark" onclick="deleteTask(${index})"></i></span>
       </li>
     `;
   });
-  listItem.innerHTML=listItem;
-}
+  listItem.innerHTML = lists;
+};
 
 taskShow();
+
+// edit list
+let edittask = (index) => {
+  let webTask = localStorage.getItem("localtask");
+  let taskObj = JSON.parse(webTask);
+  InputTask.value = taskObj[index];
+  updateBtn.style.display = "block";
+  addBtn.style.display = "none";
+  saveTask.value = index;
+};
+updateBtn.addEventListener("click", () => {
+  let webTask = localStorage.getItem("localtask");
+  let taskObj = JSON.parse(webTask);
+  let indexValue = saveTask.value;
+  taskObj[indexValue] = InputTask.value;
+  localStorage.setItem("localtask", JSON.stringify(taskObj));
+  updateBtn.style.display = "none";
+  addBtn.style.display = "block";
+  InputTask.value = "";
+  taskShow();
+});
+
+// deletetask
+let deleteTask = (index) => {
+  let webTask = localStorage.getItem("localtask");
+  let taskObj = JSON.parse(webTask);
+  taskObj.splice(index, 1);
+  localStorage.setItem("localtask", JSON.stringify(taskObj));
+  taskShow();
+};
